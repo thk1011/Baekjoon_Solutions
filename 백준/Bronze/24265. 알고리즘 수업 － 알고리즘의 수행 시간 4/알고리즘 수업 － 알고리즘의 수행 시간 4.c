f4148e0bd29aed62109831c3c811defb9ad4c6b4
thk1011 <thk1011@korea.ac.kr>
2026-03-26 12:58:11 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	long long N;
+	scanf("%lld", &N);
+	printf("%lld\n2", N*(N-1)/2);
+	
+	
+	
+	
+
+	return 0;
+}
